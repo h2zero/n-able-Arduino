ae4bd01c5e3a7179d1a69a1825eb15dda9dc557e
h2zero <powellperalta@gmail.com>
2022-07-24 08:28:29 -0600

    Redirect _write to Serial.
    Former-commit-id: 724eab0f0873602476ba072e0f7e6d29df887bd1

--- cores/nRF5/libc/printf/putchar.cpp
+++ cores/nRF5/libc/printf/putchar.cpp
@@ -1,10 +1,21 @@
 #include "Arduino.h"
 #include "printf.h"
 
-extern "C" void _putchar(const char c)
+extern "C"
+{
+
+void _putchar(const char c)
 {
     if(c == '\n') {
         Serial.write('\r');
     }
     Serial.write(c);
 }
+
+int _write (int fd, const void *buf, size_t count)
+{
+  (void) fd;
+  return (int)Serial.write((const uint8_t*)buf, count);
+}
+
+} //extern "C"
