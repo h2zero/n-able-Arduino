b978788fa4353cf76aa852e6e6c5d1846cf0b96b
John Maloney <jhmaloney@gmail.com>
2024-02-24 09:03:17 -0500

    Use the version of pulse.c from Adfruit's nRF5 framework which replaced the aassembly function countPulseASM() with a version written in C.

--- cores/nRF5/pulse.c
+++ cores/nRF5/pulse.c
@@ -23,30 +23,53 @@
 // See pulse_asm.S
 extern unsigned long countPulseASM(const volatile uint32_t *port, uint32_t bit, uint32_t stateMask, unsigned long maxloops);
 
+unsigned long countPulseASM(const volatile uint32_t *port, uint32_t bit, uint32_t stateMask, unsigned long maxloops)
+{
+  unsigned long width = 0;
+
+  // wait for any previous pulse to end
+  while ((*port & bit) == stateMask)
+    if (--maxloops == 0)
+      return 0;
+
+  // wait for the pulse to start
+  while ((*port & bit) != stateMask)
+    if (--maxloops == 0)
+      return 0;
+
+  // wait for the pulse to stop
+  while ((*port & bit) == stateMask) {
+    if (++width == maxloops)
+      return 0;
+  }
+  return width;
+}
+
 /* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
  * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
  * to 3 minutes in length, but must be called at least a few dozen microseconds
  * before the start of the pulse. */
-uint32_t pulseIn(uint32_t ulPin, uint32_t state, uint32_t timeout)
+uint32_t pulseIn(uint32_t pin, uint32_t state, uint32_t timeout)
 {
   // cache the port and bit of the pin in order to speed up the
   // pulse width measuring loop and achieve finer resolution.  calling
   // digitalRead() instead yields much coarser resolution.
   // PinDescription p = g_APinDescription[pin];
-  NRF_GPIO_Type* port = digitalPinToPort(ulPin);
-  uint32_t bit = digitalPinToBitMask(ulPin);
+  uint32_t bit = digitalPinToBitMask(pin); //p.ulPin;
   uint32_t stateMask = state ? bit : 0;
 
   // convert the timeout from microseconds to a number of times through
-  // the initial loop; it takes (roughly) 10 clock cycles per iteration.
-  uint32_t maxloops = microsecondsToClockCycles(timeout) / 10;
+  // the initial loop; it takes (roughly) 13 clock cycles per iteration.
+  uint32_t maxloops = microsecondsToClockCycles(timeout) / 13;
 
-  // count low-level loops during the pulse (or until maxLoops)
-  // a zero loopCount means that a complete pulse was not detected within the timeout
-  uint32_t loopCount = countPulseASM(&(port->IN), bit, stateMask, maxloops);
+  uint32_t width = countPulseASM(portInputRegister(digitalPinToPort(pin)), bit, stateMask, maxloops);
 
-  // convert the reading to (approximate) microseconds. The loop time as measured with an
-  // oscilloscope is 10 cycles on a BBC micro:bit 1.3 (nRF51822). There is error because the
-  // time is quantized to an integral number of loops and because interrupt may steal cycles.
-  return clockCyclesToMicroseconds(10 * loopCount);
+  // convert the reading to microseconds. The loop has been determined
+  // to be 13 clock cycles long and have about 16 clocks between the edge
+  // and the start of the loop. There will be some error introduced by
+  // the interrupt handlers.
+  if (width)
+    return clockCyclesToMicroseconds(width * 13 + 16);
+  else
+    return 0;
 }
